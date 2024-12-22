ef0c2f9b229345ab5cd7bfee74cde55fc4cb1260
Yasir <77128762+UDGOK@users.noreply.github.com>
2024-12-22 13:43:01 -0600

    Update Vercel and Next.js configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   images: {
     unoptimized: true,
@@ -11,10 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
+
+module.exports = nextConfig
